24f9807fb07d26074fc45aaf005a1d3751557941
katehuntsman <s577328@nwmissouri.edu>
2024-08-27 17:19:43 -0500

    utils_huntsman_03.py

--- utils_huntsman.py
+++ utils_huntsman.py
@@ -1,8 +1,40 @@
-''' ITERATION 2
+''' ITERATION 3
 
 Module: Huntsman Analytics - Reusable Module for My Data Analytics Projects
 
 '''
+
+#####################################
+# Declare global variables - keep byline at the end
+# We will use this information in a smarter byline
+#####################################
+
+# Boolean variable to indicate if the company has international clients
+has_international_clients: bool = True
+
+# Integer variable for the number of years in operation
+years_in_operation: int = 10
+
+# List of strings representing the skills offered by the company
+skills_offered: list = ["Data Analysis", "Machine Learning", "Business Intelligence"]
+
+# List of floats representing individual client satisfaction scores
+client_satisfaction_scores: list = [4.8, 4.6, 4.9, 5.0, 4.7]
+
+#####################################
+# Declare a global variable named byline. 
+# Make it a multiline f-string to show our information.
+#####################################
+
+byline: str = f"""
+---------------------------------------------------------
+Huntsman Analytics: Delivering Professional Insights
+---------------------------------------------------------
+Has International Clients:  {has_international_clients}
+Years in Operation:         {years_in_operation}
+Skills Offered:             {skills_offered}
+Client Satisfaction Scores: {client_satisfaction_scores}
+"""
 
 #####################################
 # Declare a global variable named byline.
